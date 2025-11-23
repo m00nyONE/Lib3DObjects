70e18535f5f590fe3fc1a2b89d5795e678f1c1de
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-11-23 01:23:15 +0100

    add Text Object

---
+++ objects/Text.lua
@@ -0,0 +1,46 @@
+local lib_name = "Lib3DObjects"
+local lib = _G[lib_name]
+
+local BaseObject = lib.BaseObject
+
+local Text = BaseObject:Subclass()
+lib.Text = Text
+
+function Text:Initialize(text, x, y, z)
+    BaseObject.Initialize(self, "Lib3DObjects_Text", self)
+    local _, pX, pY, pZ = GetUnitRawWorldPosition("player")
+    self:SetPosition(x or pX, y or pY, z or pZ)
+
+    self:SetText(text or "")
+
+    self:SetDrawDistanceMeters(75)
+    self:SetAlpha(1)
+
+    self.originalFont = self:GetFont()
+end
+
+function Text:Destroy()
+    self:SetText("")
+    self:SetFont(self.originalFont)
+    BaseObject.Destroy(self)
+end
+
+function Text:SetText(text)
+    self.Control:SetText(text)
+end
+function Text:GetText()
+    return self.Control:GetText()
+end
+function Text:SetFont(fontString)
+    self.Control:SetFont(fontString)
+end
+function Text:GetFont()
+    return self.Control:GetFont()
+end
+
+function Text:SetColor(r, g, b, a)
+    self.Control:SetColor(r, g, b, a)
+end
+function Text:GetColor()
+    return self.Control:GetColor()
+end
