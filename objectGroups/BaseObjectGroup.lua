58bf4d47a3ebe470bf9cfcad0b001334ff7b4457
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2026-01-04 20:51:46 +0100

    update BaseObjectGroup

--- objectGroups/BaseObjectGroup.lua
+++ objectGroups/BaseObjectGroup.lua
@@ -16,9 +16,8 @@ function BaseObjectGroup:Initialize(...)
     self.yaw = 0
     self.roll = 0
     self.isEnabled = true
+    self.isHidden = false
     self.scale = 1
-
-    self.visualRefPoint = {}
 
     -- Add initial members
     for i = 1, select("#", ...) do
@@ -60,11 +59,20 @@ end
 function BaseObjectGroup:IsEnabled()
     return self.isEnabled
 end
-function BaseObjectGroup:SetVisible(enabled)
+function BaseObjectGroup:SetEnabled(enabled)
     for _, member in ipairs(self.groupMembers) do
         member:SetEnabled(enabled)
     end
     self.isEnabled = enabled
+end
+function BaseObjectGroup:IsHidden()
+    return self.isHidden
+end
+function BaseObjectGroup:SetHidden(hidden)
+    for _, member in ipairs(self.groupMembers) do
+        member:SetHidden(hidden)
+    end
+    self.isHidden = not hidden
 end
 function BaseObjectGroup:SetScale(scale)
     for _, member in ipairs(self.groupMembers) do
@@ -215,7 +223,7 @@ function BaseObjectGroup:RotateToGroundNormal()
     return nil
 end
 
-
+-- TODO: this should work differently. it should rotate the whole group to face the camera/player/ground normal instead of setting each member individually. Or am i wrong here?
 function BaseObjectGroup:SetAutoRotationMode(mode)
     for _, member in ipairs(self.groupMembers) do
         member:SetAutoRotationMode(mode)
@@ -348,66 +356,15 @@ function BaseObjectGroup:MoveZ(deltaZ)
 
     self.z = self.z + deltaZ
 end
-
--- TODO: move to editor
-function BaseObjectGroup:EnableVisualReferencePoint()
-    local visualRefPoint = lib.Point:New(self.x, self.y, self.z)
-    visualRefPoint:SetLabel("RefPoint")
-    visualRefPoint:ShowPosition(true)
-    visualRefPoint:SetColor(1, 1, 0, 1)
-    visualRefPoint:AddCallback(function(marker, distanceToPlayer, distanceToCamera)
-        marker:SetPosition(self.x, self.y, self.z)
-    end)
-    self.visualRefPoint = visualRefPoint
-end
--- TODO: move to editor
-function BaseObjectGroup:DisableVisualReferencePoint()
-    if self.visualRefPoint then
-        self.visualRefPoint:Destroy()
-        self.visualRefPoint = nil
-    end
-end
+--- Set the reference point of the object group
+--- @param x number x coordinate of the reference point
+--- @param y number y coordinate of the reference point
+--- @param z number z coordinate of the reference point
 function BaseObjectGroup:SetReferencePoint(x, y, z)
     self.x, self.y, self.z = x, y, z
 end
+--- Get the reference point of the object group
+--- @return number, number, number x, y, z of the reference point
 function BaseObjectGroup:GetReferencePoint()
     return self.x, self.y, self.z
-end
-function BaseObjectGroup:GetBoundingBoxPoints()
-    -- initialize corner points if not already done. Then we can reuse them instead of returning a new table every time. This prevents a possible memory leak when this function is called often.
-    if not self.cp1 then
-        self.cp1 = {}
-        self.cp2 = {}
-        self.cp3 = {}
-        self.cp4 = {}
-        self.cp5 = {}
-        self.cp6 = {}
-        self.cp7 = {}
-        self.cp8 = {}
-    end
-
-    local cp1, cp2, cp3, cp4, cp5, cp6, cp7, cp8 = self.cp1, self.cp2, self.cp3, self.cp4, self.cp5, self.cp6, self.cp7, self.cp8
-    local minX, minY, minZ = math.huge, math.huge, math.huge
-    local maxX, maxY, maxZ = -math.huge, -math.huge, -math.huge
-
-    for _, member in ipairs(self.groupMembers) do
-        local x, y, z = member:GetFullPosition()
-        if x < minX then minX = x end
-        if y < minY then minY = y end
-        if z < minZ then minZ = z end
-        if x > maxX then maxX = x end
-        if y > maxY then maxY = y end
-        if z > maxZ then maxZ = z end
-    end
-
-    cp1[1], cp1[2], cp1[3] = minX, minY, minZ
-    cp2[1], cp2[2], cp2[3] = maxX, minY, minZ
-    cp3[1], cp3[2], cp3[3] = minX, maxY, minZ
-    cp4[1], cp4[2], cp4[3] = maxX, maxY, minZ
-    cp5[1], cp5[2], cp5[3] = minX, minY, maxZ
-    cp6[1], cp6[2], cp6[3] = maxX, minY, maxZ
-    cp7[1], cp7[2], cp7[3] = minX, maxY, maxZ
-    cp8[1], cp8[2], cp8[3] = maxX, maxY, maxZ
-
-    return cp1, cp2, cp3, cp4, cp5, cp6, cp7, cp8
 end
